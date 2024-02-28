cb8b4e7200d90854cf9762e0cc2d3975a5aef331
kotepgans <echo.kotepganzz2006@gmail.com>
2024-02-28 15:47:32 +0700

    fix-6

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -34,6 +36,9 @@
             {{ $slot }}
         </main>
     </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/livewire/post/list-post.blade.php
+++ resources/views/livewire/post/list-post.blade.php
@@ -1,8 +1,46 @@
 <div>
-    <div class="grid grid-cols-3 gap-4">
+    <div class="row gap-4">
         @foreach ($posts as $post)
-            <div class="card bg-base-100 shadow-xl">
-                <figure><img src="{{ Storage::url($post->thumbnail) }}" alt="Shoes" /></figure>
+            <div class="card bg-base-100 shadow-xl col">
+                <figure><img class="" height="200px"
+                        src="https://plus.unsplash.com/premium_photo-1681582960531-7b5de57fb276?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{{ $post->title }}</h2>
+                    <p>{{ $post->content }}</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Baca Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl col">
+                <figure><img class="" height="200px"
+                        src="https://plus.unsplash.com/premium_photo-1681582960531-7b5de57fb276?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{{ $post->title }}</h2>
+                    <p>{{ $post->content }}</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Baca Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl col">
+                <figure><img class="" height="200px"
+                        src="https://plus.unsplash.com/premium_photo-1681582960531-7b5de57fb276?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{{ $post->title }}</h2>
+                    <p>{{ $post->content }}</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Baca Selengkapnya</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl col">
+                <figure><img class="" height="200px"
+                        src="https://plus.unsplash.com/premium_photo-1681582960531-7b5de57fb276?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="Shoes" /></figure>
                 <div class="card-body">
                     <h2 class="card-title">{{ $post->title }}</h2>
                     <p>{{ $post->content }}</p>
